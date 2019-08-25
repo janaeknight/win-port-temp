b2b217cefe97eb1e096a5048bd05df763be2a1fa
janae.jpeg <52177358+janaeknight@users.noreply.github.com>
2019-08-25 15:42:57 -0500

    media update

--- java/javascript.js
+++ java/javascript.js
@@ -1,4 +1,20 @@
-<script>
-var dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleString();
-</script>
+var inactivityTime = function () {
+    var time;
+    window.onload = resetTimer;
+    // DOM Events
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+
+    function screensaver() {
+        alert("You are now logged out.")
+        //location.href = 'logout.html'
+    }
+
+    function resetTimer() {
+        clearTimeout(time);
+        time = setTimeout(screensaver, 9000)
+        // 1000 milliseconds = 1 second
+    }
+}; 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
